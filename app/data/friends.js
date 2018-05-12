484ad814fdf9024d3da635bcd31910014fdf0733
Catherine Muthoni <cdmuthoni2005@yahoo.com>
2018-05-12 10:24:05 -0400

    file updates

--- app/data/friends.js
+++ app/data/friends.js
@@ -2,41 +2,41 @@ var friendsArray = [
     {
       name: "Alex Smith",
       photo:
-        "./images/alex.jpg",
+        "./alex.jpeg",
       scores: ["5", "1", "4", "4", "5", "1", "2", "5", "4", "1"]
     },
     {
       name: "Taylor Henry",
       photo:
-        "./images/taylor.jpg",
+        "./taylor.jpeg",
       scores: ["4", "2", "5", "1", "3", "2", "2", "1", "3", "2"]
     },
     {
       name: "Stella Scott",
-      photo: "./images/stella.jpg",
+      photo: "./stella.jpeg",
       scores: ["5", "2", "2", "2", "4", "1", "3", "2", "5", "5"]
     },
     {
       name: "Roxana Sanchez",
       photo:
-        "./images/roxana.jpg",
+        "./roxana.jpeg",
       scores: ["3", "3", "4", "2", "2", "1", "3", "2", "2", "3"]
     },
     {
       name: "Jeffrey Brown",
       photo:
-        "./images/jeffery.jpg",
+        "./jeffery.jpeg",
       scores: ["4", "3", "4", "1", "5", "2", "5", "3", "1", "4"]
     },
     {
       name: "Gerald Brown",
       photo:
-        "./images/gerald.jpg",
+        "./gerald.jpeg",
       scores: ["4", "4", "2", "3", "2", "2", "3", "2", "4", "5"]
     },
     {
       name: "Frank Pitts",
-      photo: "./images/frank.jpg",
+      photo: "./frank.jpeg",
       scores: ["3", "3", "3", "3", "3", "3", "3", "3", "3", "3"]
     }
   ];
